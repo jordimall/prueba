0b310ed6fcc256fa688c311ba3ba09cce11abab6
jordimall <jordi.mallafre@hotmail.com>
2023-12-05 18:05:24 +0100

    initial commit

---
+++ src/main/java/com/pruebatecnica/www/Ex01C4JordiMallafreApplication.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01C4JordiMallafreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex01C4JordiMallafreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebatecnica/www/Ex01C4JordiMallafreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex01C4JordiMallafreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
